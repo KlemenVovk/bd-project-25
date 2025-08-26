26d06490cf9b43939804bed9cc3b3ab2878ed08c
SanchoSamba <urh.prosenc@gmail.com>
2025-08-26 09:35:19 +0200

    Vizualizacije

--- T6/T6_join.py
+++ T6/T6_join.py
@@ -1,14 +1,22 @@
 import dask.dataframe as dd
 import os
-from utils.constants import TASK2_OUT_ROOT, TASK6_OUT_ROOT
+
+# from utils.constants import TASK2_OUT_ROOT, TASK6_OUT_ROOT
+
 
 # Define paths
-yellow_path = os.path.join(TASK2_OUT_ROOT, "yellow_tripdata", "year==2023")
-fhvhv_path = os.path.join(TASK2_OUT_ROOT, "fhvhv_tripdata", "year==2023")
+# yellow_path = os.path.join(TASK2_OUT_ROOT, "yellow_tripdata", "year==2023")
+# fhvhv_path = os.path.join(TASK2_OUT_ROOT, "fhvhv_tripdata", "year==2023")
+
+yellow_path = "kafka/jupyter/data/yellow_sample.parquet"
+fhvhv_path = "kafka/jupyter/data/fhvhv_sample.parquet"
 
 # Read each set of parquet files separately
 yellow_df = dd.read_parquet(yellow_path, engine="pyarrow", gather_statistics=False)
 fhvhv_df = dd.read_parquet(fhvhv_path, engine="pyarrow", gather_statistics=False)
+
+yellow_df["source"] = "yellow"
+fhvhv_df["source"] = "fhvhv"
 
 # Align schemas
 yellow_cols = set(yellow_df.columns)
@@ -33,6 +41,7 @@ for col in all_cols:
 combined_df = dd.concat([yellow_df, fhvhv_df], axis=0, interleave_partitions=True)
 
 # Save the combined DataFrame to a new parquet file
+TASK6_OUT_ROOT = "kafka/jupyter/data/"
 output_path = os.path.join(TASK6_OUT_ROOT, "combined")
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 combined_df.to_parquet(output_path, engine="pyarrow")
